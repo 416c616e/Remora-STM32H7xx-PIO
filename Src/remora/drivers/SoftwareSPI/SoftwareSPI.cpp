bf4221ee5758c8b0b46ceefc54dffea1277c10f3
Alan Neads <alan@neadsdev.com>
2025-05-23 13:47:45 -0400

    Slow down SoftwareSPI

--- Src/remora/drivers/SoftwareSPI/SoftwareSPI.cpp
+++ Src/remora/drivers/SoftwareSPI/SoftwareSPI.cpp
@@ -14,7 +14,7 @@ SoftwareSPI::SoftwareSPI(const std::string& mosiPin, const std::string& misoPin,
       miso(misoPin, INPUT),
       clk(clkPin, OUTPUT),
       cs(nullptr),
-      delayTicks(2),
+      delayTicks(8),
       bitOrder(bitOrder),
       byteOrder(byteOrder) {
 	setSPIMode(mode);
@@ -27,7 +27,7 @@ SoftwareSPI::SoftwareSPI(const std::string& mosiPin, const std::string& misoPin,
       miso(misoPin, INPUT, PULLUP),
       clk(clkPin, OUTPUT),
       cs(new Pin(csPin, OUTPUT)), // Dynamically allocate CS pin
-      delayTicks(2),
+      delayTicks(8),
       bitOrder(bitOrder),
       byteOrder(byteOrder)
 {
